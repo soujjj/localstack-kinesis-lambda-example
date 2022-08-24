5b909f35702a0e23d02c7067ad0e40e09c1dcdf9
matsumoto <soujjj@gmail.com>
2022-08-24 16:42:33 +0900

    fix

--- consumer/index.js
+++ consumer/index.js
@@ -2,5 +2,11 @@ exports.handler = function(event, context) {
     console.log(`event = ${JSON.stringify(event)}`);
     console.log(`context = ${JSON.stringify(context)}`);
 
+    event.Records.forEach((record) => {
+        var payload = Buffer.from(record.kinesis.data, 'base64').toString();
+        console.log('record.kinesis.data: ', record.kinesis.data);
+        console.log('payload: ', payload);
+    });
+
     return `Hello World!`;
 };
